1723ad79d10531deff4198719de721adca2dac0a
baitas <baitasik27@gmail.com>
2026-01-30 11:21:55 +0500

    changes

---
+++ data_types.py
@@ -0,0 +1,16 @@
+x = list(("apple", "banana", "cherry"))
+y = tuple(("apple", "banana", "cherry"))
+z = dict(name="John", age=36)
+p = set(("apple", "banana", "cherry"))
+a = str("Hello World")
+print(x)
+print(y)
+print(z)
+print(p)
+print(a)
+ur = 1    # int
+ors = 2.8  # float
+lol = 1j   # complex
+print(type(ur))
+print(type(ors))
+print(type(lol))
