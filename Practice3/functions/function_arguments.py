ee4e3a936befa99260e764cbb307a6f3e46d6799
baitas <baitasik27@gmail.com>
2026-02-19 10:12:18 +0500

    good commit

---
+++ Practice3/functions/function_arguments.py
@@ -0,0 +1,11 @@
+def gmail(a):
+    if len(a) > 0 and all( ch in "qwertyuiopasdfghjklzxcvbnm0123456789" for ch in a ):
+            print(f"{a}@gmail.com")
+    else: 
+        print(" \033[91mInvalid name\033[0m")
+def sum(a,b): #this is a sum function , it takes values i input then sum them and print
+    print(a+b)
+a,b=map(int,input().split())
+sum(a,b)
+a = input("Enter you name: ")
+gmail(a)
