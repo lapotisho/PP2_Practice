178a9a6c9cc2824554229fa90945491dee1fa3ca
baitas <baitasik27@gmail.com>
2026-02-19 10:37:52 +0500

    another good commit

---
+++ Practice3/functions/basic_functions.py
@@ -0,0 +1,8 @@
+def print_hello_world():
+    print("hello world!")
+print_hello_world() # calls the function and the action inside of it happens 
+def world():
+    print("hello it's not a world")
+world()
+world()
+world() # it will print out hello it's not a world 3 times 
