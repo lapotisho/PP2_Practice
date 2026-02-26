ebe3c817a8b8b20fb3f12f3e6cd68a224df0fa8b
baitas <baitasik27@gmail.com>
2026-02-27 02:21:17 +0500

    math
    '

--- Practice4/mathh.py
+++ Practice4/mathh.py
@@ -14,3 +14,34 @@ print(perm(x,y)) # permutations but the elements' pos don't matter so this value
 x,y=10,-8
 print(abs(x*y))
 print(fabs(x*y)) # the same as abs but returns float 
+x = -100
+a =[]
+while x <= 100:
+    a.append(x)
+    x+=5 
+print(sum(a))
+print(min(a))
+print(max(a))
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+print(x)
+print(y)
+print(int(pow(y,x))) # 25^5
+y = 2
+x = 1
+while x < 128:
+    print(int(pow(y,x)),end=" ")
+    x*=2
+print("the last number is 64-bit")
+x=int(input("Enter radius of a circle: "))
+s = int(pow(x,2)) * pi
+print(s)
+print(ceil(s))
+print(floor(s))
+x=int(input("Enter any number below or equal 32: "))
+print(factorial(x) if x <= 32 else "the value is very big try again")
+print("\033[31menter only integers\033[0m")
+x=int(input("Enter any number : "))
+y=int(input("Enter any number : "))
+print(f"this is the remainder of x divided by y : {int(fmod(x,y))}")
+print(f"this is the GREATEST COMMON DIVISOR : {int(gcd(x,y))}")
