46fce3e2d0848b4083c6bcf45af0f1b6d87f8e8c
baitas <baitasik27@gmail.com>
2026-04-01 07:50:07 +0500

    LoL

--- Practice8/phonebook.py
+++ Practice8/phonebook.py
@@ -1,4 +1,6 @@
 from connect import get_connection
+import re 
+pattern = r"^\+77\d{9}$"
 
 
 def search():
@@ -19,18 +21,21 @@ def search():
 def add_or_update():
     name = input("Name: ")
     phone = input("Phone: ")
-    email = input("Email: ")
+    if re.fullmatch(pattern,phone):
+        email = input("Email: ")
 
-    conn = get_connection()
-    cur = conn.cursor()
+        conn = get_connection()
+        cur = conn.cursor()
 
-    cur.execute(
+        cur.execute(
         "CALL upsert_contact(%s, %s, %s)",
         (name, phone, email)
     )
-
-    conn.commit()
-    conn.close()
+        conn.commit()
+        conn.close()
+    else: 
+        print("The phone number is invalid")
+        return False
 
 
 def delete():
@@ -63,6 +68,14 @@ def pagination():
 
     conn.close()
 
+def showall():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM contacts;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    conn.close() 
 
 def menu():
     while True:
@@ -70,7 +83,8 @@ def menu():
         print("2. Add/Update")
         print("3. Delete")
         print("4. Pagination")
-        print("5. Exit")
+        print("5. Showall")
+        print("6. Exit")
 
         choice = input("Choose: ")
 
@@ -83,6 +97,8 @@ def menu():
         elif choice == "4":
             pagination()
         elif choice == "5":
+            showall()
+        elif choice == "6":
             break
 
 
